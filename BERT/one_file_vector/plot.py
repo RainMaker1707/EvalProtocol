0f0948f448a441c9f5bda40d9cae94437f1908b6
RainMaker1707 <hadrien.allegaert@student.uclouvain.be>
2024-09-25 15:04:47 +0200

    new plot for comparison

--- BERT/one_file_vector/plot.py
+++ BERT/one_file_vector/plot.py
@@ -16,7 +16,7 @@ data = pd.read_csv(args.filename)
 
 # Replotting the boxplot for HeadScore (binary) by Temp with the new data including Temp = 0.6
 plt.figure(figsize=(10, 6))
-sns.boxplot(x='PromptID', y='HeadScore', data=data, showmeans=True, meanline=True)
+sns.violinplot(x='PromptID', y='HeadScore', data=data)
 
 # Set the title and labels
 plt.title('HeadScore  vs prompt, Temp = 0.4')
@@ -24,7 +24,7 @@ plt.xlabel('Prompt ID')
 plt.ylabel('HeadScore')
 
 # Save the plot
-plt.savefig("BERT/one_file_vector/plots/HeadScoreP1.pdf", format="pdf")
+plt.savefig("BERT/one_file_vector/plots/HeadScoreP2.pdf", format="pdf")
 
 # Let's create boxplots for UT, UT1, and UT2 by Temp with the new dataset including Temp = 0.6
 
@@ -33,21 +33,21 @@ plt.figure(figsize=(12, 8))
 
 
 plt.subplot(3, 1, 1)
-sns.boxplot(x='PromptID', y='UT', data=data, showmeans=True, meanline=True)
+sns.violinplot(x='PromptID', y='UT', data=data)
 plt.title('UT vs prompt, Temp = 0.4')
 plt.xlabel('Prompt ID')
 plt.ylabel('UT')
 
 # Plot the boxplot for UT1 by Temp
 plt.subplot(3, 1, 2)
-sns.boxplot(x='PromptID', y='UT1', data=data, showmeans=True, meanline=True)
+sns.violinplot(x='PromptID', y='UT1', data=data)
 plt.title('UT1 vs prompt, Temp=0.4')
 plt.xlabel('Prompt ID')
 plt.ylabel('UT1')
 
 # Plot the boxplot for UT2 by Temp
 plt.subplot(3, 1, 3)
-sns.boxplot(x='PromptID', y='UT2', data=data, showmeans=True, meanline=True)
+sns.violinplot(x='PromptID', y='UT2', data=data)
 plt.title('UT2 vs prompt, Temp=0.4')
 plt.xlabel('Prompt ID')
 plt.ylabel('UT2')
@@ -55,4 +55,4 @@ plt.ylabel('UT2')
 
 # Adjust layout to avoid overlap
 plt.tight_layout()
-plt.savefig("BERT/one_file_vector/plots/UTP1.pdf", format="pdf")
+plt.savefig("BERT/one_file_vector/plots/UTP2.pdf", format="pdf")
